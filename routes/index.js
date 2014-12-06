c78d63b348c907fefdc27c33d47a4f7fb8e28ca8
Phil Gengler <phil@pgengler.net>
2014-12-06 14:08:08 -0500

    Work on web server part

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var arDrone = require('ar-drone');
+var drone;
+var glob;
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+  glob = "Hi";
+});
+
+router.post('/initialize', function(req, res) {
+  drone = arDrone.createClient();
+  res.send();
+});
+
+router.post('/takeoff', function(req, res) {
+  drone.land();
+  res.send('');
+});
+
+router.post('/land', function(req, res) {
+  drone.land();
+  res.send('');
 });
 
 module.exports = router;

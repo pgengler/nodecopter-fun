c9529695b8f8595249a0c8b3e9925bcb11c0ac44
Phil Gengler <phil@pgengler.net>
2014-12-06 14:08:01 -0500

    Fix rotate out/back script

--- rotate-out-and-back.js
+++ rotate-out-and-back.js
@@ -2,21 +2,21 @@ var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 
 client.takeoff(function() {
-	this.clockwise(0.5);
-	this.after(3500, function() {
-		this.stop();
-		this.front(0.25);
-		this.after(2000, function() {
-			this.stop();
-			this.clockwise(0.5);
-			this.after(3500, function() {
-				this.stop();
-				this.front(0.25);
-				this.after(2000, function() {
-					this.stop();
-					this.land();
-				});
-			});
-		});
+	client.clockwise(0.5);
+	client.after(3500, function() {
+		client.stop();
+		client.front(0.25);
+	});
+	client.after(4000, function() {
+		client.stop();
+		client.clockwise(0.5);
+	});
+	client.after(3500, function() {
+		client.stop();
+		client.front(0.5);
+	});
+	client.after(2000, function() {
+		client.stop();
+		client.land();
 	});
 });

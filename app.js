89732cca9e05157fdfe5289858bab096d58c40be
Phil Gengler <phil@pgengler.net>
2014-12-06 15:48:04 -0500

    Add web REPL-ish thing

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ pngStream.on('data', function(data) {
   var str = data.toString('base64');
   io.emit('frame', str);
 });
-client.on('navdata', console.log);
+var firstTime = true;
+client.on('navdata', function(navdata) {
+  if (firstTime) {
+    console.log(navdata);
+    firstTime = false;
+  }
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -24,10 +30,39 @@ app.get('/main.js', function(req, res){
 
 io.on('connection', function(socket) {
   socket.on('takeoff', function() {
-    client.takeoff();
+    client.takeoff(function() {
+      socket.emit('takeoff:complete');
+    });
   });
   socket.on('land', function() {
     client.land();
+  });
+  socket.on('stop', function() {
+    client.stop();
+  });
+  socket.on('front', function(speed) {
+    client.front(speed);
+  });
+  socket.on('back', function(speed) {
+    client.back(speed);
+  });
+  socket.on('clockwise', function(speed) {
+    client.clockwise(speed);
+  });
+  socket.on('counterClockwise', function(speed) {
+    client.counterClockwise(speed);
+  });
+  socket.on('up', function(speed) {
+    client.up(speed);
+  });
+  socket.on('down', function(speed) {
+    client.down(speed);
+  });
+  socket.on('left', function(speed) {
+    client.left(speed);
+  });
+  socket.on('right', function(speed) {
+    client.right(speed);
   });
 });
 
--- main.js
+++ main.js
@@ -7,6 +7,46 @@ $('button.land').click(function() {
 	return false;
 });
 
+$('button.run').click(function() {
+	var commands = $('textarea.commands').val().split(/\n/);
+	runCommands(commands);
+});
+
 socket.on('frame', function(data) {
 	$('img.frame').prop('src', 'data:image/png;base64,' + data);
 });
+
+function runCommands(commands)
+{
+	if (commands.length == 0) return;
+	var command = commands.shift();
+
+	var matches = command.match(/^sleep\s+(.+)/);
+	if (matches) {
+		var delay = matches[1];
+		setTimeout(function() {
+			runCommands(commands);
+		}, delay);
+	} else {
+		runCommand(command);
+		if (command == "takeoff") {
+			socket.on('takeoff:complete', function() {
+				runCommands(commands);
+			});
+			runCommand(command);
+		} else {
+			runCommand(command);
+			runCommands(commands);
+		}
+	}
+}
+
+function runCommand(command)
+{
+	if (command.match(/\s/)) {
+		var parts = command.split(/\s+/);
+		socket.emit(parts[0], parts[1]);
+	} else {
+		socket.emit(command);
+	}
+}
